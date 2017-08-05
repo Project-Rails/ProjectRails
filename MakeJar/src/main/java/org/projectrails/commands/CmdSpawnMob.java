3f7f4d5520e20e89f5b18c73bb04d5e5beaea2ce
IsaiahPatton <isaiahpatton1@gmail.com>
2017-08-05 14:34:57 -0400

    toUpperCase

--- MakeJar/src/main/java/org/projectrails/commands/CmdSpawnMob.java
+++ MakeJar/src/main/java/org/projectrails/commands/CmdSpawnMob.java
@@ -23,7 +23,7 @@ public class CmdSpawnMob extends RailCommand {
             }
             MC_EntityType e = MC_EntityType.PIG;
             try {
-                e = MC_EntityType.valueOf(args[0]);
+                e = MC_EntityType.valueOf(args[0].toUpperCase());
             } catch (Exception ex) {
                 plr.sendMessage(ChatColor.RED + "Unknown entity type.");
             }
